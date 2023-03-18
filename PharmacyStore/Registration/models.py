5962ff963ba13a76cf6524f96f0d0e9f7e49c195
sadikshya <sadikshya1826@gmail.com>
2023-03-18 12:28:02 +0545

    signup from create

--- PharmacyStore/Registration/models.py
+++ PharmacyStore/Registration/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Registration(models.Model):
+    full_name=models.CharField(max_length=40,null=0,default=0)
+    dob=models.DateField(max_length=20,default=0)
+    email=models.EmailField(max_length=10)
+    password=models.CharField(max_length=50,null=0,default=0)
+    cpassword=models.CharField(max_length=50,null=0,default=0)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
--- PharmacyStore/Registration/views.py
+++ PharmacyStore/Registration/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def loginForm(request):
+    data={
+        'title':'Member Registration'
+    }
+    return render(request,"Registration/login.html",data)
+
