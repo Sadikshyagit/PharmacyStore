f57c960d49ad41425dbeab5048a4fb23f7fda2b3
sadikshya <sadikshya1826@gmail.com>
2023-03-20 08:49:42 +0545

    Register Completed

--- PharmacyStore/Registration/urls.py
+++ PharmacyStore/Registration/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.loginForm,name="registration.login")
+    path('',views.loginForm,name="registration.login"),
     #There is set because it not accept duplicate
-
+    path('signup/',views.signform,name="registration.signup"),
+    path('store/',views.registrationStore,name="registration.store")
 ]
--- PharmacyStore/Registration/views.py
+++ PharmacyStore/Registration/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from Registration.models import Registration
 
 # Create your views here.
 def loginForm(request):
@@ -6,4 +9,33 @@ def loginForm(request):
         'title':'Member Registration'
     }
     return render(request,"Registration/login.html",data)
+
+def signform(request):
+    data={
+        'title':'signup'
+    }
+    return render(request,"Registration/signup.html",data)
+
+def registrationStore(request):
+    if request.method =='POST':
+        password=request.POST['password']
+        cpassword=request.POST['cpassword']
+
+        if password != cpassword:
+            return HttpResponse("Your password and confirm password doesnot match!!")
+        else:
+            registration= Registration(
+                email = request.POST['email'],
+                full_name=request.POST['full_name'],
+                dob=request.POST['dob'],
+                password=request.POST['password'],
+                cpassword=request.POST['cpassword'],
+                created_at=request.POST.get('created_at', False),
+                updated_at=request.POST.get('updated_at',False),
+
+            )
+            registration.save()
+            return redirect('registration.login')
+        return render(request,'registration/signup.html')
+            
 
